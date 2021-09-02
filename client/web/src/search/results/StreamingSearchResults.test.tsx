322fda92a1ea0014102d6f99865bceaa5c566c78
TJ Kandala <kandalatj@gmail.com>
2021-09-02 17:34:43 -0400

    update unit tests

--- client/web/src/search/results/StreamingSearchResults.test.tsx
+++ client/web/src/search/results/StreamingSearchResults.test.tsx
@@ -6,6 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { NEVER, of } from 'rxjs'
 import sinon from 'sinon'
 
+import { FlatExtensionHostAPI } from '@sourcegraph/shared/src/api/contract'
+import { pretendRemote } from '@sourcegraph/shared/src/api/util'
 import { FileMatch } from '@sourcegraph/shared/src/components/FileMatch'
 import { VirtualList } from '@sourcegraph/shared/src/components/VirtualList'
 import { GitRefType, SearchPatternType } from '@sourcegraph/shared/src/graphql-operations'
@@ -104,6 +106,7 @@ describe('StreamingSearchResults', () => {
             caseSensitive: true,
             versionContext: 'test',
             trace: undefined,
+            extensionHostAPI: Promise.resolve(pretendRemote<FlatExtensionHostAPI>({})),
         })
 
         element.unmount()
@@ -135,6 +138,7 @@ describe('StreamingSearchResults', () => {
             caseSensitive: false,
             versionContext: undefined,
             trace: undefined,
+            extensionHostAPI: Promise.resolve(pretendRemote<FlatExtensionHostAPI>({})),
         })
 
         element.unmount()
