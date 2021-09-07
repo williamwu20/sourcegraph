4e65fc945b777e5320677731995447a631c02a51
TJ Kandala <kandalatj@gmail.com>
2021-09-07 06:13:25 -0400

    update code monitor feature tour integration tests

--- client/web/src/integration/search.test.ts
+++ client/web/src/integration/search.test.ts
@@ -537,7 +537,7 @@ describe('Search', () => {
                 waitUntil: 'networkidle0',
             })
             await resetCreateCodeMonitorFeatureTour()
-            await driver.page.waitForSelector('.create-code-monitor-button', { visible: true })
+            await driver.page.waitForSelector('.search-result-match', { visible: true })
             expect(await isCreateCodeMonitorFeatureTourVisible()).toBeFalsy()
         })
 
@@ -547,7 +547,7 @@ describe('Search', () => {
                 waitUntil: 'networkidle0',
             })
             await resetCreateCodeMonitorFeatureTour()
-            await driver.page.waitForSelector('.create-code-monitor-button', { visible: true })
+            await driver.page.waitForSelector('.search-result-match', { visible: true })
             expect(await isCreateCodeMonitorFeatureTourVisible()).toBeTruthy()
         })
 
@@ -557,7 +557,7 @@ describe('Search', () => {
                 waitUntil: 'networkidle0',
             })
             await resetCreateCodeMonitorFeatureTour(false)
-            await driver.page.waitForSelector('.create-code-monitor-button', { visible: true })
+            await driver.page.waitForSelector('.search-result-match', { visible: true })
             expect(await isCreateCodeMonitorFeatureTourVisible()).toBeFalsy()
         })
     })
